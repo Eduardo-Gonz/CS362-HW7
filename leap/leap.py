7a97c2c2ee00f340375b29ae7ebd8419f66a4df8
Eduardo-Gonz <erugonz249@gmail.com>
2021-05-31 11:01:39 -0700

    Write code that passes second test in test_yeap.py

--- leap/leap.py
+++ leap/leap.py
@@ -1,5 +1,8 @@
 def is_leap_year(year):
     if(year % 4 == 0):
-        print("More work to be done")
+        if(year % 100 == 0):
+            print("More work to be done")
+        else:
+            return "{0} is a leap year!".format(year)
     else:
         return "{0} is not a leap year!".format(year)
