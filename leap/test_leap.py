0517b51aebf762b36a0a11aa78f2e6c21cbdca61
Eduardo-Gonz <erugonz249@gmail.com>
2021-05-31 10:58:25 -0700

    Write test that tests whether or not years that are divisble by 4 but not my 100 are correctly marked as leap years

--- leap/test_leap.py
+++ leap/test_leap.py
@@ -5,6 +5,7 @@ import leap
 class TestLeapYear(unittest.TestCase):
     def test_input(self):
         self.assertEqual(leap.is_leap_year(1995), "1995 is not a leap year!")
+        self.assertEqual(leap.is_leap_year(1996), "1996 is a leap year!")
 
 
 
