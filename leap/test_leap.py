0524cdd5fbd0de8a611bfcbf320391a1bc863f5a
Eduardo-Gonz <erugonz249@gmail.com>
2021-05-31 11:06:17 -0700

    Write two test cases that check whether or not years that are divsibile by 4, 100, and 400 are correctly marked as leap years and years that are divisible by 4, 100 but not 400 are correctly marked as not leap years

--- leap/test_leap.py
+++ leap/test_leap.py
@@ -6,6 +6,8 @@ class TestLeapYear(unittest.TestCase):
     def test_input(self):
         self.assertEqual(leap.is_leap_year(1995), "1995 is not a leap year!")
         self.assertEqual(leap.is_leap_year(1996), "1996 is a leap year!")
+        self.assertEqual(leap.is_leap_year(2000), "2000 is a leap year!")
+        self.assertEqual(leap.is_leap_year(2100), "2100 is not a leap year")
 
 
 
